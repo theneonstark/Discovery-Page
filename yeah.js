a456772fcba3397e9da695e9133d653b67adcf9d
StarK <mohd47149@gmail.com>
2023-03-17 00:41:53 +0530

    Improment

--- yeah.js
+++ yeah.js
@@ -1,31 +1,86 @@
-let Images = document.querySelectorAll('.img');
-let count = 0;
-Images.forEach(
-        (Image,index)=>{
-        Image.style.top = `${index*100}%`;
-        console.log(Images);
-});
-
-let ScroLL = () => {
-    Images.forEach(
-        (Image) => {
-            Image.style.transform = `translateY(-${count*100}%)`;
-        });
+let TextArray = ["Planet", "Mountain", "WildLife"];
+let TextIndex = 0;
+TextIndex = () =>{
+    TextIndex++;
+    let Change = document.querySelector("#hh1");
+    if (TextIndex < TextArray.length){
+        Change.innerHTML =`Amazing corners of the ${TextArray[TextIndex]}`;
+    }
+    else{
+        TextIndex = 0;
+        Change.innerHTML = `Amazing corners of the ${TextArray[TextIndex]}`;
+    }
 }
-
-window.onwheel = () => {
-        if(count<2){
-            count++;
-            ScroLL();
-            console.log(count);
-        }else if(count==2){
-            count=0;
-            ScroLL();
-        }else if(count==1){
-            count=-1;
-            ScroLL();
-        }
+setInterval(TextIndex,5000);
+let Index = 0;
+Hide = () => {
+    let Images;
+    let Img =document.querySelectorAll(".img");
+    for (let Images = 0 ; Images < Img.length ; Images++) {
+        Img[Images].style.opacity = 0;
+    }
+    Index++;
+    if (Index > Img.length){
+        Index = 1;
+    }
+    Img[Index - 1].style.opacity = 1;
 }
+setInterval(Hide,5000);
+let lineIndex = 0;
+lineHide = () => {
+    let lines;
+    let Line = document.querySelectorAll(".line");
+for (let lines = 0 ; lines < Line.length ; lines++) {
+    Line[lines].style.opacity = 0.2;
+}
+lineIndex++;
+    if (lineIndex > Line.length){
+        lineIndex = 1;
+    }
+    Line[lineIndex - 1].style.opacity = 1;
+}
+setInterval(lineHide,5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let Images = document.querySelectorAll('.img');
+// let count = 0;
+// Images.forEach(
+//         (Image,index)=>{
+//         Image.style.top = `${index*100}%`;
+//         console.log(Images);
+// });
+
+// let ScroLL = () => {
+//     Images.forEach(
+//         (Image) => {
+//             Image.style.transform = `translateY(-${count*100}%)`;
+//         });
+// }
+
+// window.onwheel = () => {
+//         if(count<2){
+//             count++;
+//             ScroLL();
+//             console.log(count);
+//         }else if(count==2){
+//             count=0;
+//             ScroLL();
+//         }else if(count==1){
+//             count=-1;
+//             ScroLL();
+//         }
+// }
 
 
 
